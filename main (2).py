fc1869700f6d80c19aba3093311148fac8110d14
Sahithya-2007 <23701A0296@aitsrajampet.ac.in>
2025-12-28 11:01:02 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,10 @@
+class solution:
+    def solve(self,n,A,B,C,cnt):
+        if n==1:
+            cnt[0]=cnt[0]+1
+            return cnt
+        self.solve(n-1,A,C,B,cnt)
+        self.solve(1,A,B,C,cnt)
+        self.solve(n-1,B,A,C,cnt)
+        
+      
